b17e5aec8fe2ee4a4136b45b496eef3c90358ff5
AlMLand <morlandalex@googlemail.com>
2020-08-19 13:44:09 +0200

    Update->change copy process, addet if statement

--- FirstSimpleCalculator/src/swing/youtube/tachenrechner/one/one/ImagesURLImport.java
+++ FirstSimpleCalculator/src/swing/youtube/tachenrechner/one/one/ImagesURLImport.java
@@ -1,11 +1,13 @@
 package swing.youtube.tachenrechner.one.one;
 
-import java.io.BufferedInputStream;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -17,33 +19,32 @@ public class ImagesURLImport {
 
 	private static final List<URL> LIST_URL_ADRESSE = new ArrayList<URL>();
 
-	private static File file = new File(System.getProperty("java.io.tmpdir") + "//MyOrdner_1//");
+	private static File file = new File(System.getProperty("java.io.tmpdir") + "//MyDirectoryCalculatorGitHub1985//");
 
 	static {
-		try {
-			LIST_URL_ADRESSE.add(new URL("https://img.icons8.com/material-rounded/50/000000/plus-2-math--v1.png"));
-			LIST_URL_ADRESSE.add(new URL("https://img.icons8.com/material-rounded/24/000000/filled-minus-2-math.png"));
-			LIST_URL_ADRESSE.add(new URL("https://img.icons8.com/material-rounded/24/000000/multiply-2.png"));
-			LIST_URL_ADRESSE.add(new URL("https://img.icons8.com/material-rounded/24/000000/divide-2.png"));
-			LIST_URL_ADRESSE.add(new URL("https://img.icons8.com/material-rounded/24/000000/equal-sign-2.png"));
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		}
-		
-		if(!file.exists()) {
-			file.mkdir();
-		}
-		
-		for (int i = 0; i < LIST_URL_ADRESSE.size(); i++) {
-			try (BufferedInputStream bis = new BufferedInputStream(LIST_URL_ADRESSE.get(i).openStream());
-					FileOutputStream fos = new FileOutputStream(file + "//" + ARRAY_SUFFIX[i])) {
-				byte[] array = new byte[1024];
-				int placeHolder;
-				if ((placeHolder = bis.read(array, 0, 1024)) != -1) {
-					fos.write(array, 0, placeHolder);
+		if (!file.exists()) {
+			try {
+				LIST_URL_ADRESSE.add(new URL("https://img.icons8.com/material-rounded/50/000000/plus-2-math--v1.png"));
+				LIST_URL_ADRESSE
+						.add(new URL("https://img.icons8.com/material-rounded/24/000000/filled-minus-2-math.png"));
+				LIST_URL_ADRESSE.add(new URL("https://img.icons8.com/material-rounded/24/000000/multiply-2.png"));
+				LIST_URL_ADRESSE.add(new URL("https://img.icons8.com/material-rounded/24/000000/divide-2.png"));
+				LIST_URL_ADRESSE.add(new URL("https://img.icons8.com/material-rounded/24/000000/equal-sign-2.png"));
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+			}
+
+			if (!file.exists()) {
+				file.mkdir();
+			}
+
+			for (int i = 0; i < LIST_URL_ADRESSE.size(); i++) {
+				try (InputStream inputStream = LIST_URL_ADRESSE.get(i).openStream()) {
+					Files.copy(inputStream, Paths.get(file + "//" + ARRAY_SUFFIX[i]),
+							StandardCopyOption.REPLACE_EXISTING);
+				} catch (IOException ioe) {
+					ioe.printStackTrace();
 				}
-			} catch (IOException ioe) {
-				ioe.printStackTrace();
 			}
 		}
 	}
